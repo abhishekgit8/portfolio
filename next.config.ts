98f1ece2d40527f043eefc62802e8ba8b83b0ecf
Abhishek <abhishek.radhakrishnan@calpinetech.com>
2025-04-25 13:51:47 +0530

    Commiting after removing unused code

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ["github.com", "res.cloudinary.com", "images.unsplash.com"],
-  },
-};
-
-export default nextConfig;
